da3f23b648c91bc69e37bb33ede7752fbd1277ed
ShimBeom <sbheart032@hanmail.net>
2019-06-27 08:10:48 +0900

    2

---
+++ stsTest/src/main/java/com/example/demo/model/BoardModel.java
@@ -0,0 +1,36 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+public class BoardModel {
+	private int id;
+	private String subject;
+	private String content;
+	private Date reg_date;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Date reg_date) {
+		this.reg_date = reg_date;
+	}
+	
+	
+}
